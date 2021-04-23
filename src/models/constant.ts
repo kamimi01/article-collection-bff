d9346eb1ee0c7a45eaaedbadb81a4425ee214fb9
kamimi01 <awakaru0122@gmail.com>
2021-04-23 21:03:00 +0900

    refactor: MVCアーキテクチャを適用

---
+++ src/models/constant.ts
@@ -0,0 +1,4 @@
+export namespace ArticlesConst {
+  export const QIITA_USER_NAME = "qiitaUserName";
+  export const NOTE_USER_NAME = "noteUserName";
+}
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,45 +1,5 @@
 import express from "express";
-const webclient = require("request");
 export const router = express.Router();
+import { doGetArticles } from "../controllers/articleController";
 
-router.get("/articles", (req, res) => {
-  const qiitaUserName = req.query.qiitaUserName;
-  const noteUserName = req.query.noteUserName;
-  // let response;
-
-  // QiitaのAPIを叩く
-  const qiitaBaseApi =
-    "https://qiita.com/api/v2/users/" + qiitaUserName + "/items";
-
-  webclient.get(
-    {
-      url: qiitaBaseApi,
-      qs: {
-        qiitaUserName: qiitaUserName,
-        noteUserName: noteUserName,
-      },
-    },
-    function (error: Error, response: Response, body: Body) {
-      // console.log(response.user);
-      console.log(typeof response, typeof error, typeof body);
-    }
-  );
-
-  res.json({
-    articleContents: [
-      {
-        service: "qiita",
-        title:
-          "色んな人向けにバーチャルSNS - cluster - に関するリンクを広く浅くまとめてみた",
-        userName: "kamimi01",
-        numOfGood: 1,
-        profileImageUrl:
-          "https://avatars1.githubusercontent.com/u/47489629?v=4",
-        url: "https://qiita.com/kamimi01/items/353ed9502ed62cbe9864",
-        createdDate: "2020-12-24T12:06:44+09:00",
-      },
-    ],
-  });
-});
-
-// module.exports = router;
+router.get("/", doGetArticles);
